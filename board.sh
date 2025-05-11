a4d88287810a2acbca41841d5b6d11f1e5064abf
jkauerl <kauer.javier01@gmail.com>
2025-05-11 13:56:22 -0400

    Feat: fixed

--- board.sh
+++ board.sh
@@ -2,41 +2,39 @@ clean_board() {
     if [ -d board ]; then
         cd board || return
         rm -rf *
+        cd ..
     else
         echo "Directory 'board' does not exist."
     fi
 }
 
-create_board() {
+create_board_recursive() {
     local depth=$1
     local width=$2
     local files=$3
 
-    mkdir board
-
-    cd board
-
-    for ((i = 0; i < depth; i++)); do
-        mkdir "depth_$i"
-        cd "depth_$i" || return
-
-        for ((j = 0; j < width; j++)); do
-            mkdir "width_$j"
-            cd "width_$j" || return
-
-            for ((k = 0; k < files; k++)); do
-                touch "file_$k.txt"
-            done
-
-            cd .. || return
+    if [ "$depth" -le 0 ]; then
+        for ((i = 0; i < files; i++)); do
+            touch "file_$i.txt"
         done
+        return
+    fi
 
-        cd .. || return
+    for ((i = 0; i < width; i++)); do
+        mkdir "dir$i"
+        cd "dir$i" || return
+        create_board_recursive $((depth - 1)) "$width" "$files"
+        cd ..
     done
+}
 
-    cd .. || return
+create_board() {
+    mkdir -p board
+    cd board || return
+    create_board_recursive "$@"
+    cd ..
 }
 
 clean_board
 
-create_board
+create_board 2 3 2  # depth=2, width=3, files=2
