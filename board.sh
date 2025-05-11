eed35d10ece42a899337cf23dee3ce5e72c51f15
Javier Kauer <kauer.javier01@gmail.com>
2025-05-11 12:39:46 -0400

    initial commit

---
+++ board.sh
@@ -0,0 +1,38 @@
+clean_board() {
+    if [ -d board ]; then
+        cd board || return
+        rm -rf *
+    else
+        echo "Directory 'board' does not exist."
+    fi
+}
+
+create_board() {
+    local depth = $1
+    local width = $2
+    local files = $3
+
+    mkdir board
+
+    cd board
+
+    for ((i = 0; i < depth; i++)); do
+        mkdir "depth_$i"
+        cd "depth_$i" || return
+
+        for ((j = 0; j < width; j++)); do
+            mkdir "width_$j"
+            cd "width_$j" || return
+
+            for ((k = 0; k < files; k++)); do
+                touch "file_$k.txt"
+            done
+
+            cd .. || return
+        done
+
+        cd .. || return
+    done
+
+    cd .. || return
+}
