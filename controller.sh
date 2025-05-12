d6c52a6d0f7430838ab4695134481d00a23a3388
Javier Kauer <kauer.javier01@gmail.com>
2025-05-12 18:28:36 -0400

    Feat: first implementation of controller.sh

---
+++ controller.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+count_directories() {
+    count=0
+    for directory in */; do
+        if [ -d "$directory" ]; then
+            count=$((count + 1))
+        fi
+    done
+    return $count
+}
+
+random_number() {
+    local max=$1
+    return $((RANDOM % max))
+}
+
+place_treasure() {
+    cd board || return
+
+    # Recursive function to place the treasure in a random directory
+    recursive_place() {
+        # Check if the current directory contains a file with .txt extension
+        for file in *; do
+            if [[ "$file" == *.txt ]]; then
+                # Place the treasure in this file
+                echo "Placing treasure in $file"
+                echo "Treasure!" >> "$file"
+                return
+            fi
+        done
+
+        # If no .txt file is found go deeper into the directory structure
+        # by going into one random subdirectory
+        directories=count_directories
+        next_directoyr=random_number "$directories"
+        local i=0
+        cd "$next_directory" || return
+        # Recursively call the function to place the treasure
+        recursive_place
+        cd .. || return 
+    }
+}
+
+place_treasure
