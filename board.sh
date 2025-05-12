c80a34f731a8d4362660df91422b6e4e80ed0ee1
jkauerl <kauer.javier01@gmail.com>
2025-05-11 22:57:23 -0400

    Feat: first implementation of fill_board

--- board.sh
+++ board.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+random_text() {
+    local length=$1
+    cat /dev/urandom | tr -dc 'a-Za-z0-9' | head -c "$length" 
+}
+
 clean_board() {
     cd board || return
     rm -rf *
@@ -34,6 +39,42 @@ create_board() {
     cd ..
 }
 
+fill_board() {
+    local name=$1
+    local content=$2
+    local checksum=$3
+    local encrypted=$4
+    local signed=$5
+    local length=100
+
+    fill_recursive() {
+        for file in *; do
+            if [ -d "$file" ]; then
+                cd "$file" || continue
+                fill_recursive
+                cd ..
+            elif [[ "$file" == *.txt ]]; then
+                echo "Filling $file with random content..."
+                random_text "$length" > "$file"
+
+                if [ "$checksum" = true ]; then
+                    sha256sum "$file" > "$file.sha256"
+                fi
+                if [ "$encrypted" = true ]; then
+                    openssl enc -aes-256-cbc -salt -in "$file" -out "$file.enc" -k secret
+                fi
+                if [ "$signed" = true ]; then
+                    gpg --sign "$file"
+                fi
+            fi
+        done
+    }
+
+    cd board || return
+    fill_recursive
+    cd ..
+}
+
 if [ -d board ]; then
     clean_board
     echo "Cleaned existing board directory."
@@ -41,3 +82,6 @@ fi
 
 echo "Creating board directory..."
 create_board 3 2 4
+
+echo "Filling board with random content..."
+fill_board "random_text" true true true true
