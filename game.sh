42f7c1024f1ff74009a6e9dcfc40b594359a9ed0
Javier Kauer <kauer.javier01@gmail.com>
2025-05-13 15:05:48 -0400

    Feat: implementation of the game loop

---
+++ game.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ./board.sh
+source ./controller.sh
+
+local name=$1
+local content=$2
+local checksum=$3
+local encrypted=$4
+local signed=$5
+
+if [ -d board ]; then
+    clean_board
+    echo "Cleaned existing board directory."
+fi
+
+echo "Creating board directory..."
+create_board 3 2 4
+
+echo "Filling board with random content..."
+fill_board "$name" "$content" "$checksum" "$encrypted" "$signed"
+
+echo "Placing treasure randomly..."
+read t_name t_content t_checksum t_encrypted t_signed < < (place_treasure)
+
+while true; do
+    echo "Please enter a path to validate the treasure:"
+    read -r path
+    # Check if the path is the treasure path and if so exit the loop using the function verify
+    if verify "$t_name" "$t_content" "$t_checksum" "$t_encrypted" "$t_signed"; then
+        echo "Treasure found!"
+        break
+    else
+        echo "Not the treasure path. Try again."
+    fi
+done
