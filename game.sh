a9a1444ddae52c4912a4fbb14c544985eef7b705
Javier Kauer <kauer.javier01@gmail.com>
2025-05-22 14:44:58 -0400

    Feat: correctly calling the functions for the game

--- game.sh
+++ game.sh
@@ -3,11 +3,7 @@
 source ./board.sh
 source ./controller.sh
 
-name=$1
-content=$2
-checksum=$3
-encrypted=$4
-signed=$5
+mode=$1
 
 if [ -d board ]; then
     clean_board
@@ -15,40 +11,31 @@ if [ -d board ]; then
 fi
 
 echo "Creating board directory..."
-create_board 1 2 2
+# Parameters are: depth, width, files
+create_board 1 2 2  
 
 echo "Filling board with random content..."
-fill_board "$name" "$content" "$checksum" "$encrypted" "$signed"
+fill_board "$mode"
 
 echo "Placing treasure randomly..."
-declare -a treasure_info
-place_treasure treasure_info
-
-t_name=${treasure_info[0]}
-t_content=${treasure_info[1]}
-t_checksum=${treasure_info[2]}
-t_encrypted=${treasure_info[3]}
-t_signed=${treasure_info[4]}
+t_key=$(place_treasure "$mode")
 
 while true; do
     echo "Please enter a path (board/ is already included) to validate the treasure:"
     read -r path
-declare -a verify_result
-    if verify "$path" verify_result; then
-        p_name="${verify_result[0]}"
-        p_content="${verify_result[1]}"
-        p_checksum="${verify_result[2]}"
-        p_encrypted="${verify_result[3]}"
-        p_signed="${verify_result[4]}"
 
-        if [ "$p_name" = "$t_name" ] && 
-        [ "$p_content" = "$t_content" ] && \
-        [ "$p_checksum" = "$t_checksum" ] && \
-        [ "$p_encrypted" = "$t_encrypted" ] && \
-        [ "$p_signed" = "$t_signed" ]; then
-            echo "Treasure found!"
-            break
-        fi
+    # For current mode, pick corresponding treasure_info element as key:
+    case "$mode" in
+        name) key="${t_key}" ;;
+        content) key="${t_key}" ;;
+        checksum) key="${t_key}" ;;
+        encrypted) key="${t_key}" ;;
+        signed) key="${t_key}" ;;
+    esac
+
+    if verify "$path" "$mode" "$key"; then
+        echo "Treasure found!"
+        break
     else
         echo "Not the treasure path. Try again."
     fi
